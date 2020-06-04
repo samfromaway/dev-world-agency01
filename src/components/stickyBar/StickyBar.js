55059dbd9f6f368ccc3250672a32db37a03320cf
samfromaway@gmail.com <email@example.com>
2020-06-04 14:19:29 -0500

    sticky bar update

--- src/components/stickyBar/StickyBar.js
+++ src/components/stickyBar/StickyBar.js
@@ -6,10 +6,6 @@ import './sticky-bar.css';
 const StickyBar = pros => {
   return (
     <section className="sticky-bar">
-      <div className="swiss-info">
-        <img src={SwissFlag} alt="Swiss-Flag" className="swiss-flag" />
-        <h5 className="swiss-title">Swiss Quality</h5>
-      </div>
       <h3 className="title01">DEV WORLD DIGITAL AGENCY</h3>
       <ButtonInt
         text="CONTACT"
