ab78194bcf7f12a3f3afd1b003878e2d560b0074
samfromaway@gmail.com <email@example.com>
2020-06-05 10:14:26 -0500

    sass conversion

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,26 +5,14 @@
  */
 
 // You can delete this file if you're not using it
-import './src/styles/1-base/global.css';
-import './src/styles/1-base/_global-test.scss';
-import './src/styles/1-base/typography.css';
-////
-////
-
+import './src/styles/1-base/typography.scss';
 // 2-Layout
 import './src/styles/2-layout/anchor.css';
-import './src/styles/2-layout/footer.css';
-import './src/styles/2-layout/section01.css';
+import './src/styles/2-layout/section01.scss';
 import './src/styles/2-layout/contact-section.css';
-import './src/styles/2-layout/cards.css';
+import './src/styles/2-layout/cards.scss';
 // 3-Components
-import './src/styles/3-components/cardsglow01.css';
 import './src/styles/3-components/iconcontent01.css';
-import './src/styles/3-components/icons.css';
-import './src/styles/3-components/icons02.css';
-import './src/styles/3-components/imgglow01.css';
-import './src/styles/3-components/smallcards.css';
-import './src/styles/3-components/videos01.css';
 import './src/styles/3-components/wrapreverse01.css';
 // 4-Animation
 import './src/styles/4-animations/appearanim01.css';
