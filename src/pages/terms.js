984482c0aec762114eb8bf8de850c027988c4534
samfromaway@gmail.com <email@example.com>
2020-06-06 01:47:15 -0500

    terms

---
+++ src/pages/terms.js
@@ -0,0 +1,109 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+import PageHeader from '../components/pageHeader/PageHeader';
+
+const Terms = () => (
+  <Layout>
+    <SEO title="Terms" />
+    <PageHeader title="Terms" />
+    <div className="section pure-text-section">
+      <h3>Privacy</h3>
+      <p>
+        We will not share your email address with anyone else. When you
+        subscribe to our Newsletter your email address is placed in a database.
+        You can remove your address from our emailing list by clicking the
+        respective link at the end of each email we send. We will not add your
+        name to our email list without your permission.
+      </p>
+      <h3>Personal Information</h3>
+      <p>
+        We will never ask you for personal information–your credit card number,
+        for example–except when you place an order. At that time, we will
+        provide a secure (SSL, encrypted) connection and require you to submit
+        only the information needed to complete and fulfill your order. This
+        information will be used to process your order, providing compliance
+        with proper and safe data processing by PayPal or other third party
+        processors. PayPal also provides our customers with secure privacy
+        protection. We will not sell or rent your personal or credit information
+        to anyone, nor share it in any other way, unless required to by a law
+        enforcement agency that takes the proper steps to acquire it. We follow
+        all legal requirements to protect your privacy. Personally Identifiable
+        Information may include information such as your name, email address,
+        and other related information that you have provided to us or that we
+        obtain about you. It is only used with your consent, allowing us to
+        explicitly provide your with legitimate interests the content, paid or
+        otherwise, which you have requested. All data is used under the expected
+        behavior of business interest and with customer data privacy at the
+        forefront. All information you’ve provided can be provided to you at
+        your request (under GDPR guidelines).
+      </p>
+      <h3>Cookies</h3>
+      <p>
+        Our website(s) use “cookies” to identify the pages and articles that
+        visitors have read. What is a cookie? – A cookie is a tiny data file
+        that is stored on your web browser and typically includes non personal
+        information about your visit. This non personal information is not
+        personally identifiable to you. Why do we use cookies? – As is common
+        practice with almost all professional websites this site uses cookies.
+        We use them to make sure our users are having their interests and needs
+        met by our content. Cookies help to tell us if, in general, our readers
+        enjoyed a piece of content and stayed to read it or if the content
+        wasn’t good or out of date and visitors are not enjoying what they’re
+        reading. Cookies are overall used in an effort to improve the user
+        experience of our website. We use cookies for a variety of reasons
+        detailed below. <br />
+        <br /> Disabling Cookies – You can prevent the setting of cookies by
+        adjusting the settings on your browser (see your browser Help for how to
+        do this). Be aware that disabling cookies will affect the functionality
+        of this and many other websites that you visit. Disabling cookies will
+        usually result in also disabling certain functionality and features of
+        the this site. Therefore it is recommended that you do not disable
+        cookies. ​ The Cookies We Set​ In some special cases we also use cookies
+        provided by trusted third parties. The following section details which
+        third party cookies you might encounter through this site. This site
+        uses Google Analytics which is one of the most widespread and trusted
+        analytics solution on the web for helping us to understand how you use
+        the site and ways that we can improve your experience. These cookies may
+        track things such as how long you spend on the site and the pages that
+        you visit so we can continue to produce engaging content. We have set
+        our Google Analytics data to expire after 38 months. <br /> <br /> For
+        more information on Google Analytics cookies, see the official Google
+        Analytics page. We also use social media buttons and/or plugins on this
+        site that allow you to connect with your social network in various ways.
+        For these to work the following social media sites including; Facebook,
+        Twitter, and Google+, will set cookies through our site which may be
+        used to enhance your profile on their site or contribute to the data
+        they hold for various purposes outlined in their respective privacy
+        policies.
+      </p>
+      <h3>Using, Selling, Publishing Contents Of Our Website</h3>
+      <p>
+        The website and its contents are owned or licensed by the website.
+        Material contained on the website must be presumed to be proprietary and
+        copyrighted. Visitors have no rights whatsoever in the site content. Use
+        of website content for any reason is unlawful unless it is done with
+        express contract or permission of the publisher.
+      </p>
+      <h3>Guarantee of Content</h3>
+      <p>
+        We don’t guarantee that any of the content of that website is accurate.
+        We are not liable for any misinformation.
+      </p>
+      <h3>Responsability</h3>
+      <p>
+        The terms and conditions are of Roellin Services under the label of Dev
+        World and Dev World Digital Agency as a sole proprietorship from
+        Switzerland. Sonnhaldenstrasse, 6331 Huenenberg which provides the
+        platform. The website is therefore copyrighted by Roellin Services. ​
+        How To Medellin is a joint venture between Vero International S.A.S,
+        Expat Solutions and Roellin Services.
+      </p>
+      <h3></h3>
+      <p></p>
+    </div>
+  </Layout>
+);
+
+export default Terms;
