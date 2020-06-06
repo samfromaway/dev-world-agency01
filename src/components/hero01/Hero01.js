c7ab49a40565bead28c22c22875c3c86d30e1029
samfromaway@gmail.com <email@example.com>
2020-06-05 20:09:57 -0500

    hero adj

--- src/components/hero01/Hero01.js
+++ src/components/hero01/Hero01.js
@@ -7,7 +7,7 @@ const Hero01 = () => {
       <div className="content-section1">
         <h3 className="title08">WE BUILD</h3>
         <h1 className="title02">A WEB PRESENCE</h1>
-        <h1 className="title02">YOU ARE PROUD OF</h1>
+        <h1 className="title02">TO BE PROUD OF</h1>
         <h3 className="title07">Passionate | Efficient | Resourceful</h3>
       </div>
     </section>
