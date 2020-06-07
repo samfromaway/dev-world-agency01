6ca8493fa0eb894b64ef87330816faac3acb6271
samfromaway@gmail.com <email@example.com>
2020-06-07 15:57:54 -0500

    about us finish

--- src/data/team.js
+++ src/data/team.js
@@ -1,9 +1,12 @@
+import UserMale from '../images/user.jpg';
+import UserFemale from '../images/user2.jpg';
+import Coco from '../images/coco.jpg';
+
 const team = [
   {
     id: 1,
     title: 'Ana Caceres',
-    img:
-      'https://image.cnbcfm.com/api/v1/image/105595209-1543339207911gettyimages-1069148086.jpeg?v=1543339234',
+    img: UserFemale,
     subTitle: 'Quality Control',
     desc:
       'Ana is responsible for Quality Control. Her eye for detail finds even the tiniest flaws, so we are able to provide you with a polished product.',
@@ -11,8 +14,7 @@ const team = [
   {
     id: 2,
     title: 'Orlando Gonzalez',
-    img:
-      'https://image.cnbcfm.com/api/v1/image/105595209-1543339207911gettyimages-1069148086.jpeg?v=1543339234',
+    img: UserMale,
     subTitle: 'Translation',
     desc:
       'Orlando is our main translator for English to Spanish. His command of both languages allows us to reach the Latin American market.',
@@ -20,8 +22,7 @@ const team = [
   {
     id: 3,
     title: 'Coco',
-    img:
-      'https://image.cnbcfm.com/api/v1/image/105595209-1543339207911gettyimages-1069148086.jpeg?v=1543339234',
+    img: Coco,
     subTitle: 'Office Dog',
     desc:
       'Coco is our office dog who reminds us to value the little things. He is the most important team member and our unique selling point.',
