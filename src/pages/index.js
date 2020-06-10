e3a36242a7fc15ff00d437a781cabcc8792ecc29
samfromaway@gmail.com <email@example.com>
2020-06-10 07:13:46 -0500

    bug fix2?

--- src/pages/index.js
+++ src/pages/index.js
@@ -21,7 +21,61 @@ import WebLogos from '../images/WebLogos.png';
 import MarketingLogos from '../images/MarketingLogos.png';
 import DesignLogos from '../images/DesignLogos.png';
 //data
-import testimonials from '../data/testimonials';
+
+import JakeImg from '../images/testimonials/jake.jpg';
+import WalterImg from '../images/testimonials/walter.jpg';
+import You from '../images/testimonials/you.jpg';
+
+const testimonials = [
+  {
+    id: 1,
+    title: 'Jake Johnson',
+    img: JakeImg,
+    subTitle: 'United States',
+    desc:
+      'It is great working with Sam, he is very thorough. I really like his attention to detail. I highly recommend working with him.',
+  },
+  {
+    id: 2,
+    title: 'Kreativgarasch',
+    img: WalterImg,
+    subTitle: 'Switzerland',
+    desc:
+      'I am very happy about the work Sam and Tacha consistently do for me. You can feel, they really enjoy their work.',
+  },
+  {
+    id: 3,
+    title: 'You?',
+    img: You,
+    subTitle: 'International',
+    desc:
+      'We would love to have you as our next happy client. Send us a message with your project and we get going.',
+  },
+  {
+    id: 4,
+    title: 'Jake Johnson',
+    img: JakeImg,
+    subTitle: 'United States',
+    desc:
+      'It is great working with Sam, he is very thorough. I really like his attention to detail. I highly recommend working with him.',
+  },
+  {
+    id: 5,
+    title: 'Kreativgarasch',
+    img: WalterImg,
+    subTitle: 'Switzerland',
+    desc:
+      'I am very happy about the work Sam and Tacha consistently do for me. You can feel, they really enjoy their work.',
+  },
+  {
+    id: 6,
+    title: 'You?',
+    img: You,
+    subTitle: 'International',
+    desc:
+      'We would love to have you as our next happy client. Send us a message with your project and we get going.',
+  },
+];
 
 const IndexPage = () => (
   <Layout>
