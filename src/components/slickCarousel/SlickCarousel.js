77f20e59406f76ddeebbd04434dc8bffbb315f35
samfromaway@gmail.com <email@example.com>
2020-06-10 07:19:50 -0500

    bug fix?

--- src/components/slickCarousel/SlickCarousel.js
+++ src/components/slickCarousel/SlickCarousel.js
@@ -19,7 +19,7 @@ class SimpleSlider extends React.Component {
         {
           breakpoint: 1000,
           settings: {
-            //  slidesToShow: 2,
+            slidesToShow: 2,
             // slidesToScroll: 1,
             // swipeToSlide: true,
           },
