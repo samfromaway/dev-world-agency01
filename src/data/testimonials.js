32ea4bd2fe73065b403329d52b17ba4dc4a926f5
samfromaway@gmail.com <email@example.com>
2020-06-14 09:01:26 -0500

    testimonail add

--- src/data/testimonials.js
+++ src/data/testimonials.js
@@ -1,6 +1,7 @@
 import JakeImg from '../images/testimonials/jake.jpg';
 import WalterImg from '../images/testimonials/walter.jpg';
 import You from '../images/testimonials/you.jpg';
+import FabianImg from '../images/testimonials/fabian.jpg';
 
 const testimonials = [
   {
@@ -29,27 +30,10 @@ const testimonials = [
   },
   {
     id: 4,
-    title: 'Jake Johnson',
-    img: JakeImg,
-    subTitle: 'United States',
-    desc:
-      'It is great working with Sam, he is very thorough. I really like his attention to detail. I highly recommend working with him.',
-  },
-  {
-    id: 5,
-    title: 'Kreativgarasch',
-    img: WalterImg,
+    title: 'Fabian Hofstetter',
+    img: FabianImg,
     subTitle: 'Switzerland',
-    desc:
-      'I am very happy about the work Sam and Tacha consistently do for me. You can feel, they really enjoy their work.',
-  },
-  {
-    id: 6,
-    title: 'You?',
-    img: You,
-    subTitle: 'International',
-    desc:
-      'We would love to have you as our next happy client. Send us a message with your project and we get going.',
+    desc: 'Great work on my logo. Extremely fast and competent.',
   },
 ];
 
