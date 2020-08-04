e482b7aa3af3ba68513196aed3dde843ee4b9c34
samfromaway@gmail.com <email@example.com>
2020-08-04 17:13:51 -0500

    test form fix

--- src/components/quiz/ProgressBar.js
+++ src/components/quiz/ProgressBar.js
@@ -2,22 +2,9 @@ import React from 'react';
 import './progressBar.scss';
 
 const ProgressBar = props => {
-  const progressStyle = () => {
-    switch (props.step) {
-      case 1:
-        return 10;
-      case 2:
-        return 50;
-      case 3:
-        return 94;
-      default:
-        10;
-    }
-  };
-
   return (
     <div className="progressBar__wrapper">
-      <div className="progressBar" style={{ width: `${progressStyle()}%` }} />
+      <div className="progressBar" style={{ width: `${20}%` }} />
     </div>
   );
 };
